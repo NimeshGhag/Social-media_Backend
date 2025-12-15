22f25bc0a4e262d2791074120f5b7cb17d9d3fab
NimeshGhag <ghagnimesh490@gmail.com>
2025-12-15 11:59:32 +0530

    feat: add MongoDB connection using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
 const app = require("./src/app");
+const connectToDB = require("./src/db/db");
 const port = process.env.PORT;
+
+connectToDB();
 
 app.listen(port, () => {
   console.log("Server is running on port");
