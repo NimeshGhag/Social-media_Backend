898e3d81ba026544b414e86c1826a4e5f468e01f
NimeshGhag <ghagnimesh490@gmail.com>
2025-12-23 12:45:09 +0530

    feat: auto-generate caption for post using AI

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,13 +1,18 @@
-
+const genrateCaption = require("../services/ai.service");
 
 const createPostController = async (req, res) => {
   try {
     const file = req.file;
 
+    const base64Image = new Buffer.from(file.buffer).toString("base64");
+
     const { prompt } = req.body;
+
+    const caption = await genrateCaption(base64Image, prompt);
 
     return res.status(200).json({
       message: "Create post route working 👍",
+      caption,
     });
   } catch (error) {
     return res.status(500).json({
